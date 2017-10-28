067db130995e155ade9a1b936bca04771d8c5d12
Quinn Wilson <github@midworld.org>
2017-10-28 20:58:36 +0000

    working in happy path

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,18 +1,40 @@
 module Lib
     ( someFunc
-    
+    , findPeaks
     , pickPeaks
-    , average
-    
+    , PickedPeaks(..)
     ) where
 data PickedPeaks = PickedPeaks { pos :: [Int], peaks :: [Int]} deriving (Eq, Show)
 
 pickPeaks :: [Int] -> PickedPeaks
-pickPeaks a = PickedPeaks a a--set =filter (\(x, index) -> x > (average set))  zip set [1..]
+--pickPeaks a = PickedPeaks a a--set =filter (\(x, index) -> x > (average set))  zip set [1..]
+pickPeaks [] = PickedPeaks [] [] --filter (\(x, index) -> x > (average set))  zip set [1..]
+pickPeaks set = wrap $ 
+                    unzip $
+                    map (\((_,peak,_),idx)-> (peak,idx)) $ 
+                    filter (\((x,y,z),idx) -> y > x && y > z) $  
+                    zip (tri set) [1..]
+    where dedup = filter(\(x,y) -> x /= y) $ zip set $ tail set
+          wrap s = PickedPeaks (snd s) (fst s)
+          tri a = zip3 a (tail a) ( tail $ tail a )
 
-average :: (Fractional a, Foldable t) => t a -> a
-average set = (sum set) / fromIntegral (length set)
+-- let set =  [3,2,3,6,4,1,2,3,2,1,2,3]
+-- let dedup = filter (\(x,y,idx) -> x /= y) $ zip3 set (tail set) [0..]
 
+-- below works but not with plateaus
+-- filter (\((x,y,z),idx) -> y > x && y > z)$  zip (tri set) [1..]
+-- map (\((_,peak,_),idx)-> (peak,idx)) $ filter (\((x,y,z),idx) -> y > x && y > z)$  zip (tri set) [1..]
+
+
+
+
+
+findPeaks :: (Int, Int) -> [(Int,Int)] -> [(Int,Int)]
+findPeaks (x, index) [] = [(x,index)]
+findPeaks (x, index) ((y,yindex):acc) = if ( x > y ) then ((x,index):acc) else ((y,yindex):acc) 
+
+
+--let set2 = zipWith3 (\y x z -> (x-y, z)) set  (tail set) [1..] 
 -- your code here
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,13 @@ import Lib
 
 main :: IO ()
 main = hspec $ do
-    describe "average" $ do 
-        it "should average 3 items" $ do 
-            average [1, 2, 3] `shouldBe` 2
+    describe "Sample test" $ do
+        it "[3,2,3,6,4,1,2,3,2,1,2,3]" $ do
+            let myPeak = PickedPeaks {pos = [3,7], peaks = [6,3]}
+            pickPeaks [3,2,3,6,4,1,2,3,2,1,2,3] `shouldBe` myPeak        
+        it "[3,2,3,6,4,8,2,3,2,1,2,3]" $ do
+            let myPeak = PickedPeaks {pos = [3,5,7], peaks = [6,8,3]}
+            pickPeaks [3,2,3,6,4,8,2,3,2,1,2,3] `shouldBe` myPeak        
+--        it "[1,2,2,1]" $ do
+--            let myPeak = PickedPeaks {pos = [1], peaks = [2]}
+--            pickPeaks [1,2,2,1] `shouldBe` myPeak        
