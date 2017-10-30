e7a7d89b8982debbfb91a168ca1a84af93a3ca46
Quinn Wilson <github@midworld.org>
2017-10-30 04:30:32 +0000

    workin

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,40 +1,23 @@
 module Lib
     ( someFunc
-    , findPeaks
     , pickPeaks
+    , findPeaks
     , PickedPeaks(..)
     ) where
 data PickedPeaks = PickedPeaks { pos :: [Int], peaks :: [Int]} deriving (Eq, Show)
 
 pickPeaks :: [Int] -> PickedPeaks
---pickPeaks a = PickedPeaks a a--set =filter (\(x, index) -> x > (average set))  zip set [1..]
-pickPeaks [] = PickedPeaks [] [] --filter (\(x, index) -> x > (average set))  zip set [1..]
-pickPeaks set = wrap $ 
-                    unzip $
-                    map (\((_,peak,_),idx)-> (peak,idx)) $ 
-                    filter (\((x,y,z),idx) -> y > x && y > z) $  
-                    zip (tri set) [1..]
-    where dedup = filter(\(x,y) -> x /= y) $ zip set $ tail set
-          wrap s = PickedPeaks (snd s) (fst s)
-          tri a = zip3 a (tail a) ( tail $ tail a )
+pickPeaks [] = PickedPeaks [] []
+pickPeaks (x:xs) = (\(peak,_,pos)->PickedPeaks (map (+1) pos) peak) 
+    $ unzip3 $ findPeaks (x,0) (zip3 xs (tail xs ++ [last xs]) [1..])
 
--- let set =  [3,2,3,6,4,1,2,3,2,1,2,3]
--- let dedup = filter (\(x,y,idx) -> x /= y) $ zip3 set (tail set) [0..]
-
--- below works but not with plateaus
--- filter (\((x,y,z),idx) -> y > x && y > z)$  zip (tri set) [1..]
--- map (\((_,peak,_),idx)-> (peak,idx)) $ filter (\((x,y,z),idx) -> y > x && y > z)$  zip (tri set) [1..]
-
-
-
-
-
-findPeaks :: (Int, Int) -> [(Int,Int)] -> [(Int,Int)]
-findPeaks (x, index) [] = [(x,index)]
-findPeaks (x, index) ((y,yindex):acc) = if ( x > y ) then ((x,index):acc) else ((y,yindex):acc) 
-
-
---let set2 = zipWith3 (\y x z -> (x-y, z)) set  (tail set) [1..] 
+findPeaks:: (Int,Int) -> [(Int,Int,Int)] -> [(Int,Int,Int)]
+findPeaks s [] = []
+findPeaks (prev,prevIdx) ((curr,next,idx):acc)
+    | prev < curr && curr > next  = (curr,next,prevIdx) : findPeaks (curr,idx) acc
+    | curr < next =  findPeaks (curr,idx) acc
+    | otherwise =  findPeaks (prev,prevIdx) acc
+    
 -- your code here
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,13 @@ import Lib
 
 main :: IO ()
 main = hspec $ do
+    describe "findPeaks" $ do 
+        it "[3,2,3,6,4,1,2,3,2,1,2,3]" $ do
+            let set = [3,2,3,6,4,1,2,3,2,1,2,3] 
+            findPeaks ((head set),0) (zip3 set (tail set) [1..])  `shouldBe` [(6,4,3),(3,2,7)]
+--        it "[1,2,2,1]" $ do
+--            let set = [1,2,2,1]
+--            peakTest ((head set),0) (zip3 set (tail set) [0..])  `shouldBe` [(2,1,2)]
     describe "Sample test" $ do
         it "[3,2,3,6,4,1,2,3,2,1,2,3]" $ do
             let myPeak = PickedPeaks {pos = [3,7], peaks = [6,3]}
@@ -10,6 +17,15 @@ main = hspec $ do
         it "[3,2,3,6,4,8,2,3,2,1,2,3]" $ do
             let myPeak = PickedPeaks {pos = [3,5,7], peaks = [6,8,3]}
             pickPeaks [3,2,3,6,4,8,2,3,2,1,2,3] `shouldBe` myPeak        
---        it "[1,2,2,1]" $ do
---            let myPeak = PickedPeaks {pos = [1], peaks = [2]}
---            pickPeaks [1,2,2,1] `shouldBe` myPeak        
+        it "[1,2,2,1]" $ do
+            let myPeak = PickedPeaks {pos = [1], peaks = [2]}
+            pickPeaks [1,2,2,1] `shouldBe` myPeak        
+        it "[1,2,2,2,2,2,2,1]" $ do
+            let myPeak = PickedPeaks {pos = [1], peaks = [2]}
+            pickPeaks [1,2,2,2,2,2,2,1] `shouldBe` myPeak        
+        it "[0, 1, 2, 5, 1, 0]" $ do 
+            let myPeak = PickedPeaks {pos = [3], peaks = [5]}
+            pickPeaks [0, 1, 2, 5, 1, 0] `shouldBe` myPeak
+        it "[0,2,2,2]" $ do 
+            let myPeak = PickedPeaks {pos = [], peaks = []}
+            pickPeaks [0, 2, 2, 2] `shouldBe` myPeak
